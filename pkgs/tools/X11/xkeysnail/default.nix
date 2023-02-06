8d39137e6d8781023816d9caa0c1214a69835726
bb2020 <bb2020@users.noreply.github.com>
2023-02-06 11:27:47 +0300

    xkeysnail: refactor

--- pkgs/tools/X11/xkeysnail/default.nix
+++ pkgs/tools/X11/xkeysnail/default.nix
@@ -1,17 +1,16 @@
-{ lib, fetchFromGitHub, makeWrapper, python3Packages }:
+{ lib, fetchFromGitHub, python3Packages }:
 
 python3Packages.buildPythonApplication rec {
   pname = "xkeysnail";
-  version = "0.4.0";
+  version = "0.4";
 
   src = fetchFromGitHub {
     owner = "mooz";
     repo = pname;
     rev = "bf3c93b4fe6efd42893db4e6588e5ef1c4909cfb";
-    sha256 = "0plcpb4ndzfsd5hj32m0g32swnhyph9sd759cdhhzmjvlq3j8q6p";
+    hash = "sha256-12AkB6Zb1g9hY6mcphO8HlquxXigiiFhadr9Zsm6jF4=";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
   propagatedBuildInputs = with python3Packages; [
     evdev
     xlib
@@ -19,15 +18,10 @@ python3Packages.buildPythonApplication rec {
     appdirs
   ];
 
-  doCheck = false;
-
   postInstall = ''
     mkdir -p $out/share
-    cp ./example/config.py $out/share/example.py
-    cp ${./browser-emacs-bindings.py} $out/share/browser.py
+    cp ${./emacs.py} $out/share/browser.py
 
-    makeWrapper $out/bin/xkeysnail $out/bin/xkeysnail-example \
-      --add-flags "-q" --add-flags "$out/share/example.py"
     makeWrapper $out/bin/xkeysnail $out/bin/xkeysnail-browser \
       --add-flags "-q" --add-flags "$out/share/browser.py"
   '';
