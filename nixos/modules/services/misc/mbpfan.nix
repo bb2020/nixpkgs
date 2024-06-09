df18f4ae7721582639d5a25c3ff2f0fb1c94ad3c
bb2020 <bb2020@users.noreply.github.com>
2024-06-09 17:26:09 +0300

    nixos/mbpfan: make use of mkEnableOption

--- nixos/modules/services/misc/mbpfan.nix
+++ nixos/modules/services/misc/mbpfan.nix
@@ -8,63 +8,42 @@ let
   cfgfile = format.generate "mbpfan.ini" cfg.settings;
 
 in {
-  options.services.mbpfan = {
-    enable = mkEnableOption "mbpfan, fan controller daemon for Apple Macs and MacBooks";
-    package = mkPackageOption pkgs "mbpfan" {};
+  options.services.mbpfan.enable = mkEnableOption "mbpfan, fan controller daemon for Apple Macs and MacBooks";
+  options.services.mbpfan.verbose = mkEnableOption "set the log level to verbose";
+  options.services.mbpfan.aggressive = mkEnableOption "favor higher default fan speeds" // { default = true; };
+  options.services.mbpfan.package = mkPackageOption pkgs "mbpfan" {};
 
-    verbose = mkOption {
-      type = types.bool;
-      default = false;
-      description = "If true, sets the log level to verbose.";
-    };
+  options.services.mbpfan.settings = mkOption {
+    default = {};
+    description = "";
+    type = types.submodule {
+      freeformType = format.type;
 
-    aggressive = mkOption {
-      type = types.bool;
-      default = true;
-      description = "If true, favors higher default fan speeds.";
-    };
-
-    settings = mkOption {
-      default = {};
-      description = "INI configuration for Mbpfan.";
-      type = types.submodule {
-        freeformType = format.type;
-
-        options.general.low_temp = mkOption {
-          type = types.int;
-          default = (if cfg.aggressive then 55 else 63);
-          defaultText = literalExpression "55";
-          description = "If temperature is below this, fans will run at minimum speed.";
-        };
-        options.general.high_temp = mkOption {
-          type = types.int;
-          default = (if cfg.aggressive then 58 else 66);
-          defaultText = literalExpression "58";
-          description = "If temperature is above this, fan speed will gradually increase.";
-        };
-        options.general.max_temp = mkOption {
-          type = types.int;
-          default = (if cfg.aggressive then 78 else 86);
-          defaultText = literalExpression "78";
-          description = "If temperature is above this, fans will run at maximum speed.";
-        };
-        options.general.polling_interval = mkOption {
-          type = types.int;
-          default = 1;
-          description = "The polling interval.";
-        };
+      options.general.low_temp = mkOption {
+        type = types.int;
+        default = (if cfg.aggressive then 55 else 63);
+        defaultText = literalExpression "55";
+        description = "If temperature is below this, fans will run at minimum speed.";
+      };
+      options.general.high_temp = mkOption {
+        type = types.int;
+        default = (if cfg.aggressive then 58 else 66);
+        defaultText = literalExpression "58";
+        description = "If temperature is above this, fan speed will gradually increase.";
+      };
+      options.general.max_temp = mkOption {
+        type = types.int;
+        default = (if cfg.aggressive then 78 else 86);
+        defaultText = literalExpression "78";
+        description = "If temperature is above this, fans will run at maximum speed.";
+      };
+      options.general.polling_interval = mkOption {
+        type = types.int;
+        default = 1;
+        description = "The polling interval.";
       };
     };
   };
-
-  imports = [
-    (mkRenamedOptionModule [ "services" "mbpfan" "pollingInterval" ] [ "services" "mbpfan" "settings" "general" "polling_interval" ])
-    (mkRenamedOptionModule [ "services" "mbpfan" "maxTemp" ] [ "services" "mbpfan" "settings" "general" "max_temp" ])
-    (mkRenamedOptionModule [ "services" "mbpfan" "lowTemp" ] [ "services" "mbpfan" "settings" "general" "low_temp" ])
-    (mkRenamedOptionModule [ "services" "mbpfan" "highTemp" ] [ "services" "mbpfan" "settings" "general" "high_temp" ])
-    (mkRenamedOptionModule [ "services" "mbpfan" "minFanSpeed" ] [ "services" "mbpfan" "settings" "general" "min_fan1_speed" ])
-    (mkRenamedOptionModule [ "services" "mbpfan" "maxFanSpeed" ] [ "services" "mbpfan" "settings" "general" "max_fan1_speed" ])
-  ];
 
   config = mkIf cfg.enable {
     boot.kernelModules = [ "coretemp" "applesmc" ];
